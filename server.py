d9143cf21af408e9ec647c16b010fa5f21602f07
Harshit M <79850332+djharshit@users.noreply.github.com>
2024-12-30 15:22:16 +0530

    save to SQLite database with improved query and timeout handling

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+
+""" This script fetches the TV schedule from the website and saves it to a SQLite database. """
+
 import sqlite3
 import sys
 from datetime import date
@@ -10,6 +14,7 @@ from faker import Faker
 
 
 def save_to_db():
+    """Fetches the TV schedule from the website and saves it to a SQLite database."""
     URL = "https://tvschedule.today/in/tv-schedule/romedy-now"
     HEADERS: dict[str, str] = {"User-Agent": Faker().chrome()}
 
@@ -17,16 +22,25 @@ def save_to_db():
     cur = conn.cursor()
 
     try:
-        response = requests.get(url=URL, headers=HEADERS)
+        response = requests.get(url=URL, headers=HEADERS, timeout=10)
         response.raise_for_status()
 
         soup = BeautifulSoup(response.text, "html.parser")
 
-        names = soup.find("section", attrs={"class": "grid gap-6 bg-primary rounded-2xl"}).find_all("h3")
+        names = soup.find(
+            "section", attrs={"class": "grid gap-6 bg-primary rounded-2xl"}
+        ).find_all("h3")
 
         for i, j in enumerate(names):
-            query = "INSERT OR IGNORE INTO movies (date, name) VALUES (?, ?);"
+            query = """
+                INSERT INTO movies (date, name)
+                SELECT ?, ?
+                WHERE NOT EXISTS (
+                    SELECT 1 FROM movies WHERE name = ?
+                );
+            """
             cur.execute(query, (date.today(), j.text.strip()))
+            conn.commit()
 
             print(date.today(), i + 1, "done")
 
@@ -45,7 +59,7 @@ def save_to_db():
 
 
 scheduler = BlockingScheduler()
-trigger = CronTrigger(hour=13, minute=36, second=0)
+trigger = CronTrigger(hour=1, minute=36, second=0)
 
 scheduler.add_job(save_to_db, trigger)
 
